37eaa42f1df8e2fa31a9e40cdc77193f3507b19a
juancamilorivas <bogota.diamond@hotmail.com>
2023-03-15 17:00:27 -0500

    npm run build generated

---
+++ .next/static/l4ZzaaKQS6xmGRx5s-jmg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/375-2adbbfd0321637f3.js","static/chunks/pages/index-7b113ec14dcd1a20.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
