51ae44262d4ddf5f90fe4478f9adadc30b7a78dd
Camilo Rivas <67404147+juancamilorivas@users.noreply.github.com>
2024-09-14 11:18:42 -0500

    google tag

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
+      <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5PZQ95PC');</script>
+<!-- End Google Tag Manager -->
       <link rel="icon" href="../public/favicon.png" legacyBehavior/>
         <meta content="Camilo's Portafolio" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css" legacyBehavior/>
@@ -12,6 +19,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5PZQ95PC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
       </body>
     </Html>
   );
