7fac5b224f73eae049b39e4dab4d15bdb7c319dc
Juan Camilo Rivas molina <jrivas@seeed.us>
2024-09-14 12:25:27 -0500

    tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import 'bootswatch/dist/cosmo/bootstrap.min.css'
-import '../global.css'
-import Head from 'next/head'
-
+import 'bootswatch/dist/cosmo/bootstrap.min.css';
+import '../global.css';
+import Head from 'next/head';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,9 +9,12 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Camilo's Portafolio</title>
       </Head>
+      {/* Google Tag Manager */}
+      <GoogleTagManager gtmId="GTM-5PZQ95PC" />
+      {/* Renderización del componente actual */}
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { GoogleTagManager } from '@next/third-parties/google'
 
 
 export default function Document() {
   return (
     <Html>
       <Head>
-      <GoogleTagManager gtmId="GTM-5PZQ95PC" />
         <link rel="icon" href="../public/favicon.png" legacyBehavior />
         <meta content="Camilo's Portafolio" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css" legacyBehavior />
